e40b3cfc2220d695b2bfcc05e3061166633aaf53
Elchin Abuzarli <abuzerli.dev@gmail.com>
2024-04-30 11:52:28 +0400

    skills

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <section>
       <Hero/>
       <About/>
-      {/* <Skill/> */}
+      <Skill/>
       {/* <Projects/> */}
       {/* <Experience/> */}
       {/* <Contact/> */}
