2cc4f6b21a3e44ee0c39f51a064ba98f31de6868
Elchin Abuzarli <abuzerli.dev@gmail.com>
2024-08-01 13:38:33 +0400

    adding image

--- components/about/about.jsx
+++ components/about/about.jsx
@@ -1,15 +1,24 @@
+import Image from 'next/image'
 import './about.scss'
+import me from '@/assets/men.png'
 
 function About() {
   return (
     <section id='about' className='about-container'>
       <div className="about-wrapper">
-        <div className="about-left"></div>
+        <div className="about-left">
+          <Image
+          className='my-img'
+          src={me}
+          alt='me'
+          />
+        </div>
         <div className="about-right">
             <span className='about'>About me</span>
             <h1 className='about-title'>Transforming visions into exceptional portfolios</h1>
             <p className='about-txt'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat blanditiis, suscipit fugiat vel explicabo facilis sed porro quos cum consequatur fuga architecto veritatis nemo ipsa deserunt eius perspiciatis earum temporibus. Ea delectus suscipit hic eos reiciendis, voluptatum maiores, cumque natus pariatur odio ipsa neque inventore esse nam assumenda! Blanditiis, nesciunt.
+            I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.
+
             </p>
         </div>
       </div>
