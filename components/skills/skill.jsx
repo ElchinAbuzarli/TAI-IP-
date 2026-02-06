800d7e9d38add33eb77bd5b80b42fa99ceaac7a1
ElchinAbuzarli <abuzerli.dev@gmail.com>
2026-02-06 13:09:24 +0400

    Update assets and enhance skills component layout; added new images for Electron, MySQL, PHP, Tailwind, Tauri, and TypeScript, while removing the old 'me.png' image. Improved styling for skills display with additional flex containers.

--- components/skills/skill.jsx
+++ components/skills/skill.jsx
@@ -13,6 +13,12 @@ import Git from "../../assets/Git.png";
 import MUI from "../../assets/mui.png";
 import Andtd from "../../assets/antd.png";
 import Bootstrap from "../../assets/bootstrap.png";
+import Electron from "../../assets/electron.png";
+import MySQL from "../../assets/mysql.png";
+import PHP from "../../assets/php.png";
+import Tailwind from "../../assets/tailwind.png";
+import Tauri from "../../assets/tauri.png";
+import TypeScript from "../../assets/ts.png";
 
 
 
@@ -28,9 +34,38 @@ function Skill() {
         </h1>
 
         <div className="all-skills">
-
+          {/* 1-ci sıra: ən az (1 dənə) */}
           <div className="first-skills">
-            
+            {/* React */}
+            <div className="skills">
+              <Image src={React} alt="skill" width={35} height={35} />
+              <span>React</span>
+            </div>
+          </div>
+
+          {/* 2-ci sıra: bir az çox (3 dənə) */}
+          <div className="second-skills">
+            {/* JavaScript */}
+            <div className="skills">
+              <Image src={JavaScript} alt="skill" width={35} height={35} />
+              <span>Javascript</span>
+            </div>
+
+            {/* TypeScript */}
+            <div className="skills">
+              <Image src={TypeScript} alt="skill" width={35} height={35} />
+              <span>TypeScript</span>
+            </div>
+
+            {/* Next.js */}
+            <div className="skills">
+              <Image src={NEXTJS} alt="skill" width={35} height={35} />
+              <span>Next.js</span>
+            </div>
+          </div>
+
+          {/* 3-cü sıra: daha çox (4 dənə) */}
+          <div className="third-skills">
             {/* HTML */}
             <div className="skills">
               <Image src={HTML} alt="skill" width={35} height={35} />
@@ -49,52 +84,55 @@ function Skill() {
               <span>SASS</span>
             </div>
 
-            {/* JAVASCRIPT */}
+            {/* Tailwind */}
             <div className="skills">
-              <Image src={JavaScript} alt="skill" width={35} height={35} />
-              <span>Javascript</span>
+              <Image src={Tailwind} alt="skill" width={35} height={35} />
+              <span>Tailwind</span>
             </div>
+          </div>
 
-            {/* REACT */}
-            <div className="skills">
-              <Image src={React} alt="skill" width={35} height={35} />
-              <span>React</span>
-            </div>
-
-            {/* NEXT JS */}
-            <div className="skills">
-              <Image src={NEXTJS} alt="skill" width={35} height={35} />
-              <span>Next.js</span>
-            </div>
-
-            {/* NODE JS */}
+          {/* 4-cü sıra: daha çox (5 dənə) */}
+          <div className="fourth-skills">
+            {/* Node.js */}
             <div className="skills">
               <Image src={NODEJS} alt="skill" width={55} height={40} />
               <span>Node.js</span>
             </div>
-          </div>
 
-          <div className="second-skills">
-
-            {/* MONGODB */}
+            {/* MongoDB */}
             <div className="skills">
               <Image src={MongoDB} alt="skill" width={35} height={35} />
               <span>MongoDB</span>
             </div>
 
-            {/* POSTMAN */}
+            {/* MySQL */}
+            <div className="skills">
+              <Image src={MySQL} alt="skill" width={35} height={35} />
+              <span>MySQL</span>
+            </div>
+
+            {/* PHP */}
+            <div className="skills">
+              <Image src={PHP} alt="skill" width={35} height={35} />
+              <span>PHP</span>
+            </div>
+
+            {/* Postman */}
             <div className="skills">
               <Image src={Postman} alt="skill" width={35} height={35} />
               <span>Postman</span>
             </div>
+          </div>
 
-            {/* GIT */}
+          {/* 5-ci sıra: ən çox (6 dənə, baza) */}
+          <div className="fifth-skills">
+            {/* Git */}
             <div className="skills">
               <Image src={Git} alt="skill" width={35} height={35} />
               <span>Git</span>
             </div>
 
-            {/* MUI */}
+            {/* Material UI */}
             <div className="skills">
               <Image src={MUI} alt="skill" width={35} height={35} />
               <span>Material UI</span>
@@ -112,6 +150,17 @@ function Skill() {
               <span>AntD</span>
             </div>
 
+            {/* Electron */}
+            <div className="skills">
+              <Image src={Electron} alt="skill" width={35} height={35} />
+              <span>Electron</span>
+            </div>
+
+            {/* Tauri */}
+            <div className="skills">
+              <Image src={Tauri} alt="skill" width={35} height={35} />
+              <span>Tauri</span>
+            </div>
           </div>
         </div>
       </div>
